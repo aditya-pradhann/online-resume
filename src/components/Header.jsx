d6b670ffd1825844fa17a6975e9fe0f51f875054
Aditya Pradhan <adityaa.pradhan@outlook.com>
2024-04-30 22:21:47 +0545

    email id added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import {
   faLinkedin,
   faGithub,
 } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 import "../styles/style.scss";
 
@@ -16,6 +17,7 @@ export const Header = () => {
     },
     { iconName: faGithub, link: "https://github.com/aditya-pradhann" },
     { iconName: faXTwitter, link: "https://twitter.com/AdiCodes07" },
+    { iconName: faEnvelope, mailTo: "adityaa.pradhan@outlook.com" },
   ];
 
   return (
@@ -24,16 +26,35 @@ export const Header = () => {
         <h1>Aditya Pradhan</h1>
         <h2>Senior Software Engineer</h2>
         <p>
-          Experienced Software Developer with a proven track record of successfully driving and contributing to diverse projects across various domains. Adept at translating project requirements into tangible solutions, optimizing codebase quality, and fostering collaborative environments.
+          Experienced Software Developer with a proven track record of
+          successfully driving and contributing to diverse projects across
+          various domains. Adept at translating project requirements into
+          tangible solutions, optimizing codebase quality, and fostering
+          collaborative environments.
         </p>
       </div>
 
       <ul className="flex p-0">
         {social.map((value, index) => (
           <li className="shrink-0 mr-5" key={index}>
-            <a className="social-link" href={value.link} target="_blank" rel='noreferrer'>
-              <FontAwesomeIcon icon={value.iconName} size="2xl" />
-            </a>
+            {value.link ? (
+              <a
+                className="social-link"
+                href={value.link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={value.iconName} size="2xl" />
+              </a>
+            ) : (
+              <a
+                className="social-link"
+                href={`mailto:${value.mailTo}`}
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={value.iconName} size="2xl" />
+              </a>
+            )}
           </li>
         ))}
       </ul>
